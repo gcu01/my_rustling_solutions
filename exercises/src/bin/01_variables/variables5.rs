99939c56bd59f8e5c79081b660fb4e6c8de59f3d
Gabriel <gabrielcampeanu@gmail.com>
2024-09-07 21:40:09 +0200

    more on shadowing

--- exercises/src/bin/01_variables/variables5.rs
+++ exercises/src/bin/01_variables/variables5.rs
@@ -5,4 +5,12 @@ fn main() {
     // TODO: Fix the compiler error by changing the line below without renaming the variable.
     let number = 3;
     println!("Number plus two is: {}", number + 2);
+    let n1:i32 = number;
+    println!("n1 is: {}", n1);
+    consume(number);
+    println!("Number plus two is: {}", number + 2);
+}
+
+fn consume(n: i32) {
+    println!("I am consuming the {}", n);
 }
--- exercises/src/bin/01_variables/variables6.rs
+++ exercises/src/bin/01_variables/variables6.rs
@@ -3,4 +3,10 @@ const NUMBER: usize = 3;
 
 fn main() {
     println!("Number: {NUMBER}");
+    {
+        const NR1: bool = true;
+        println!("NR1 and Number = {},{}", NR1, NUMBER );
+    }
+    // NR1 is not in this scope
+    // println!("NR1 and Number = {},{}", NR1, NUMBER );
 }
